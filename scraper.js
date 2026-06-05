f55e988dd3ec9fac724a0a919f2e9edaa4eb95bd
Suhana  harishankar Tiwari <suhanatiwari943@gmail.com>
2026-06-05 21:51:14 +0530

    This change the 1web page

--- scraper.js
+++ scraper.js
@@ -10,7 +10,7 @@ async function scrape() {
     try {
         const response = await axios.get(url, {
             headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36' },
-            timeout: 20000
+            timeout: 100000
         });
 
         const $ = cheerio.load(response.data);
